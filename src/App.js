f7b49be81aceefc2c37386d7b93c6f5a16f46e93
yeonhwa <yeonhwa6717@gmail.com>
2023-05-15 23:36:24 +0900

    Set initial setting

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">REACT-CLONE-YOUTUBE</header>
+    </div>
+  );
+}
+
+export default App;
