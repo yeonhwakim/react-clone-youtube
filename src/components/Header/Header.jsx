5b3ca038c36b3d4972cbe1624ee5e64859b5638e
yeonhwa <yeonhwa6717@gmail.com>
2023-05-17 23:48:36 +0900

    feat: Add handleChange to unControlled component.
    Add a searchKeyword state using useState hook.
    Add a handleChange function to handle input changes and update seatchKeyword state.
    Assign the value and onChange props to the input tag.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Header() {
+  const [searchKeyword, setSearchKeyword] = useState("");
+
+  const handleChange = (e) => {
+    setSearchKeyword(e.target.value);
+  };
+
   return (
     <div>
       <div>logo</div>
       <form>
-        <input type="text" placeholder="Search" />
+        <input
+          type="text"
+          placeholder="Search"
+          value={searchKeyword}
+          onChange={handleChange}
+        />
         <button type="submit">SEARCH</button>
       </form>
     </div>
