2bbf0ab051aaff0839c5dc3b1487dad1eda2bd18
yeonhwa <yeonhwa6717@gmail.com>
2023-05-17 23:45:15 +0900

    style: Markup basic header layout.
    Add the logo section.
    Add a from tag with input and button tags for releated search.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <div>
+      <div>logo</div>
+      <form>
+        <input type="text" placeholder="Search" />
+        <button type="submit">SEARCH</button>
+      </form>
+    </div>
+  );
 }
 
 export default Header;
