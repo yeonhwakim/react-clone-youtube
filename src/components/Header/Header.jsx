48989ef83a2b2582f00fc61afb8dcdd71dedc1e9
yeonhwa <yeonhwa6717@gmail.com>
2023-05-17 23:58:48 +0900

    feat: Add handleSubmit to route search page.
    Add a handleSubmit function to handle the form submission and route to the search result page.
    Assign the handleSubmit function as a prop to the form tag.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,23 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Header() {
+  const navigate = useNavigate();
   const [searchKeyword, setSearchKeyword] = useState("");
 
   const handleChange = (e) => {
     setSearchKeyword(e.target.value);
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    navigate(`/videos/${searchKeyword}`);
+  };
+
   return (
     <div>
       <div>logo</div>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           type="text"
           placeholder="Search"
