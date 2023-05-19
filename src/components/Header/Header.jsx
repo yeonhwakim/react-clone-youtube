19e04d854931926e1f44c37e14e03d1abea6fe13
yeonhwa <yeonhwa6717@gmail.com>
2023-05-19 22:56:35 +0900

    style: Add header component style.
    Add a logo.
    Add the input and button style using tailwindcss class.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,6 +6,9 @@ import {
   useParams,
 } from "react-router-dom";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+
 function Header() {
   const navigate = useNavigate();
   const { pathname } = useLocation();
@@ -32,16 +35,25 @@ function Header() {
   };
 
   return (
-    <div>
-      <div>logo</div>
-      <form onSubmit={handleSubmit}>
+    <div className="flex items-center justify-center bg-zinc-950 h-20">
+      <img src="/assets/images/logo.png" alt="메인로고" className="w-20" />
+      <form
+        className="flex items-center justify-center w-5/12"
+        onSubmit={handleSubmit}
+      >
         <input
           type="text"
           placeholder="Search"
           value={searchKeyword}
           onChange={handleChange}
+          className="h-15 placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-3 pr-3 focus:outline-none sm:text-sm"
         />
-        <button type="submit">SEARCH</button>
+        <button type="submit" className="p-4">
+          <FontAwesomeIcon
+            icon={faMagnifyingGlass}
+            className="text-white w-6 h-6"
+          />
+        </button>
       </form>
     </div>
   );
