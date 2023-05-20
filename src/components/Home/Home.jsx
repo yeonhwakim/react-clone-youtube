664d40aa15021e2c046763fc57b5af09eac71f37
yeonhwa <yeonhwa6717@gmail.com>
2023-05-20 16:31:36 +0900

    feat: Render most popular videos in the 'Home' component.
    Fetch the most popular videos using 'useFetchVoideos' hook.
    Render the 'EMPTY' string if the length of 'mostPopularVideos' is zero.
    Otherwise, render the 'Video' component for eatch videos in the 'mostPopularVideos'.

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,45 @@
 import React from "react";
 
+import useFetchVideos from "../../hooks/use-fetch-videos";
+
+import Video from "../Video/Video";
+
 function Home() {
-  return <div>Home</div>;
+  const mostPopularVideos = useFetchVideos("most");
+
+  return (
+    <>
+      {!mostPopularVideos.length && "EMPTY"}
+      {mostPopularVideos.length && (
+        <ul>
+          {mostPopularVideos.map(
+            ({
+              id,
+              snippet: {
+                thumbnails: {
+                  default: { url },
+                },
+                title,
+                publishedAt,
+                channelId,
+                channelTitle,
+              },
+            }) => (
+              <li key={id}>
+                <Video
+                  url={url}
+                  title={title}
+                  channelTitle={channelTitle}
+                  publishedAt={publishedAt}
+                  channelId={channelId}
+                />
+              </li>
+            )
+          )}
+        </ul>
+      )}
+    </>
+  );
 }
 
 export default Home;
