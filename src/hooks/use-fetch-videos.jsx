1245860669280cc476115ac7c8c1a8038bacd241
yeonhwa <yeonhwa6717@gmail.com>
2023-05-21 13:46:46 +0900

    feat: Add 'search' type.
    Add a new 'search' type for fetching data base on keyword paremater.
    Retrive the 'keyword' parameter using the 'useParams' hook.
    Include the 'keyword' in the dependency array of the 'useEffect' hook
    to trigger data fetching when keyword changes.

--- src/hooks/use-fetch-videos.jsx
+++ src/hooks/use-fetch-videos.jsx
@@ -1,13 +1,21 @@
 import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 function useFetchVideos(type) {
+  const { keyword } = useParams();
   const [videos, setVideos] = useState([]);
 
   useEffect(() => {
-    fetch(type === "most" && "data/most-popular-videos.json")
+    fetch(
+      type === "most"
+        ? "/data/most-popular-videos.json"
+        : type === "search"
+        ? `/data/search-${keyword}-videos.json`
+        : ""
+    )
       .then((res) => res.json())
       .then((data) => setVideos(data.items));
-  }, [type]);
+  }, [type, keyword]);
 
   return videos;
 }
